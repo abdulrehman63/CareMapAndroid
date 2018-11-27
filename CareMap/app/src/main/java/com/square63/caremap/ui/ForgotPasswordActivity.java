e95b24332965d80bf4178cc45849d385c8f46461
Abdul Rehman <abdul.rehman@square63.org>
2018-11-27 17:21:13 +0500

    Designed Signup, login and forgot password screen

---
+++ CareMap/app/src/main/java/com/square63/caremap/ui/ForgotPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.square63.caremap.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.square63.caremap.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
